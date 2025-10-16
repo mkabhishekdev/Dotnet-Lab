dbe36ffd38625ea0dab0f94219502db1b2351a62
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2025-10-16 20:51:11 +0530

    web api work

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -26,6 +26,21 @@ namespace api.Repository
             return commentModel;   
         }
 
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var commentModel = await _applicationDbContext.Comments.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (commentModel == null)
+            {
+                return null;
+            }
+
+            _applicationDbContext.Comments.Remove(commentModel);
+            await _applicationDbContext.SaveChangesAsync();
+
+            return commentModel;
+        }
+
         public async Task<List<Comment>> GetAllAsync()
         {
             return await _applicationDbContext.Comments.ToListAsync();
@@ -34,6 +49,23 @@ namespace api.Repository
         public async Task<Comment?> GetByIdAsync(int id)
         {
             return await _applicationDbContext.Comments.FindAsync(id);
+        }
+
+        public async Task<Comment?> UpdateAsync(int id, Comment commentModel)
+        {
+            var existingComment = await _applicationDbContext.Comments.FindAsync(id);
+
+            if (existingComment == null)
+            {
+                return null;
+            }
+
+            existingComment.Title = commentModel.Title;
+            existingComment.Content = commentModel.Content;
+
+            await _applicationDbContext.SaveChangesAsync();
+
+            return existingComment;
         }
     }
 }
