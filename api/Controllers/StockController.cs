8d82a7f41b92462f6e91b014d1c8200c9e470e22
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2025-10-17 14:49:54 +0530

    web api project updated - delete, data annotations,filtering

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -9,6 +9,7 @@ using api.Dtos.Stock;
 using Microsoft.EntityFrameworkCore;
 using api.Repository;
 using api.Interfaces;
+using api.Helpers;
 
 /* Main idea for having Controllers is to manipulating the URLs & should never have any calls to database, to allow
 separation of concerns we use the 'Repository pattern' - adding dependency injections using constructors. 99% of the
@@ -30,18 +31,28 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery]QueryObject query)
         {
-            var stocks = await _stockRepo.GetAllAsync();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            var stocks = await _stockRepo.GetAllAsync(query);
 
             var stockDto = stocks.Select(s => s.ToStockDto());
 
             return Ok(stocks);
         }
  
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
             var stock = await _stockRepo.GetByIdAsync(id);
 
             if (stock == null)
@@ -56,15 +67,25 @@ namespace api.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
             var stockModel = stockDto.ToStockFromCreateDto();
             await _stockRepo.CreateAsync(stockModel);
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
 
         [HttpPut]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
             var stockModel = await _stockRepo.UpdateAsync(id, updateDto);
 
             if (stockModel == null)
@@ -76,9 +97,14 @@ namespace api.Controllers
         }
 
         [HttpDelete]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
             var toDeleteStock = await _stockRepo.DeleteAsync(id);
 
             if (toDeleteStock == null)
--- api/Dtos/Stock/UpdateStockRequestDto.cs
+++ api/Dtos/Stock/UpdateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,22 @@ namespace api.Dtos.Stock
 {
     public class UpdateStockRequestDto
     {
+        [Required]
+        [MaxLength(10, ErrorMessage = "Symbol cannot be over 10 characters")]
         public string Symbol { get; set; } = string.Empty; // to avoid getting no assignment error 
-
+        [Required]
+        [MaxLength(10, ErrorMessage = "Company cannot be over 10 characters")]
         public string CompanyName { get; set; } = string.Empty;
-
+        [Required]
+        [Range(1, 1000000)]
         public decimal Purchase { get; set; }
-
+        [Required]
+        [Range(0.001, 100)]
         public decimal LastDividend { get; set; }
-
+        [Required]
+        [MaxLength(10, ErrorMessage = "Industry cannot be over 10 characters")]
         public string Industry { get; set; } = string.Empty;
-
+        [Range(1,50000)]
         public long MarketCap { get; set; } 
     }
 }
