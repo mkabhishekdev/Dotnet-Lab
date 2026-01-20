cea8b1912485b33e613cde3180e39b7b47ac9d97
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2026-01-20 21:31:24 +0530

    js work

--- FrontEndDevProofOfConcepts/JavascriptReview/main.js
+++ FrontEndDevProofOfConcepts/JavascriptReview/main.js
@@ -109,22 +109,51 @@ addNums(11,9);
 
 //OOP in JS
 
+/*
 // Constructor function
 function Person(firstName, lastName, dob)
 {
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = new Date(dob);
-    this.getBirthYear = function()
-    {
-        return this.dob.getFullYear();
-    }
+ 
     this.getFullName = function() 
     {
         return `${this.firstName} ${this.lastName}`;
     }
 }
 
+
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+} 
+
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+*/
+
+//Class
+class Person
+{
+    constructor(firstName, lastName, dob)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);
+    }
+    
+    getBirthYear()
+    {
+        return this.dob.getFullYear();
+    }
+
+    getFullName()
+    {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+}
 
 // Instantiate object
 const person1 = new Person('John', 'Doe', '4-3-1980');
